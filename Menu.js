34953ec8101348b64281a130527a5d25988396f8
ayunas <ayunas@gmail.com>
2019-06-20 04:08:29 -0500

    got nav to slide in and out using keyframes in mixins, and toggling classes using the event handler

--- Menu.js
+++ Menu.js
@@ -2,6 +2,6 @@ const hamburger = document.querySelector('#hamburger');
 const menu = document.querySelector('.menu');
 
 hamburger.addEventListener('click', () => {
-    menu.classList.toggle('hidden');
+    menu.classList.toggle('hideNav');
 })
 
